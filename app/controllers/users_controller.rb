d7b434afb4790ce7c829bad97a27baddeb17f928
Tam Vu <vutam_a@hotmail.com>
2020-01-07 00:52:25 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
-    if @user #&& @user.authenticate(params[:password])
+    if @user != nil#&& @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect "/tweets"
     else
